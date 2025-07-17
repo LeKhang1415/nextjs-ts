cdead7f0aa31a29eb502623ad3e25237085e235e
khang <chaoskid1415@gmail.com>
2025-07-17 15:49:06 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import Navbar from "../components/Navbar";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Next.js Project",
+    description: "A Next.js project with TypeScript and TailwindCSS.",
+    keywords: "Next.js, Typescript, TailwindCSS",
+};
 
 export default function RootLayout({
     children,
@@ -8,7 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>
+            <body className={inter.className}>
                 <Navbar />
                 <main className="max-w-3xl mx-auto">{children}</main>
             </body>
